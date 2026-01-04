6ac421cde750238e8ba00bfc626fd800f2f04f0c
Rsenatic20 <inred.rmontes@gmail.com>
2026-01-03 21:55:37 -0500

    Create frontend/src/App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Aqua Factura</h1>
+        <p>Invoice Management System</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
